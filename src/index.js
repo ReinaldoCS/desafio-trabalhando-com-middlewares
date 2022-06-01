5890ae6cbe4c626fa2145630c2574673bcf592d2
Reinaldo Correia <santosdoscorreiareinaldo@gmail.com>
2022-06-01 20:50:02 -0300

    Finalizando desafio

--- src/index.js
+++ src/index.js
@@ -11,18 +11,87 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const usernameAlreadyExists = users.find((user) => user.username === username);
+
+  if (!usernameAlreadyExists) {
+    return response.status(404).json({ error: 'Username already not exists' });
+  };
+
+  request.user = usernameAlreadyExists;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'Exceeded the limit of todos to free plan' });
+  };
+
+  if (!user.pro && user.todos.length < 10) {
+    next();
+  };
+
+  if (user.pro) {
+    next();
+  };
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'UUID not valid' });
+  }
+
+  const usernameAlreadyExists = users.find((user) => user.username === username);
+
+  if (!usernameAlreadyExists) {
+    return response.status(404).json({ error: 'Username not found' });
+  };
+
+  const todoAlreadyExists = usernameAlreadyExists.todos.find((todo) => todo.id === id);
+  
+  if (!todoAlreadyExists) {
+    return response.status(404).json({ error: 'Todo not found by id' });
+  };
+
+  request.user = usernameAlreadyExists;
+  request = request.todo = todoAlreadyExists;
+  next();
+
+  // if (validate(id)) {
+
+  //   const todoAlreadyExists = usernameAlreadyExists.todos.find((todo) => todo.id === id);
+  
+  //   if (!todoAlreadyExists) {
+  //     return response.status(404).json({ error: 'UUID not valid' });
+  //   };
+
+  //   request = request.todo = todoAlreadyExists;
+  //   next();
+  // };
+
+  // return response.status(400).json({ error: 'UUID not valid' });
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+
+  const { id } = request.params;
+  const userAlreadyExists = users.find((user) => user.id === id);
+
+  if (!userAlreadyExists) {
+    return response.status(404).json({ error: 'Username already not exists' });
+  };
+
+  request.user = userAlreadyExists;
+  next();
 }
 
 app.post('/users', (request, response) => {
@@ -32,7 +101,7 @@ app.post('/users', (request, response) => {
 
   if (usernameAlreadyExists) {
     return response.status(400).json({ error: 'Username already exists' });
-  }
+  };
 
   const user = {
     id: uuidv4(),
